5f0bafd6b1c41e0944bf05644b6670b5f7abdf16
Ashref <ashref.abderrahmane.berbaoui@ensia.edu.dz>
2025-10-29 17:02:13 +0100

    feat: Implement Custom Button and Input components

---
+++ components/ui/index.ts
@@ -0,0 +1,7 @@
+/**
+ * Centralized export for all UI components
+ */
+// Add more component exports here as you create them, examlpe: 
+export { Button } from './button';
+export { CustomButton } from './custom-button';
+export { Input } from './input';
